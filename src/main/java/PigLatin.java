caf6716207f9930a750ef97f6f88038ae308bad7
Artem Synytsia <synytsia.artem@yahoo.com>
2023-07-31 17:00:03 +0000

    WkEight_CodingChallenge_One

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -17,6 +17,12 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+        if (in == null || in.isEmpty()) {
+            return in;
+        }
+
+        char firstChar = in.charAt(0);
+        String restOfWord = in.substring(1);
+        return restOfWord + firstChar + "ay";
     }
 }
